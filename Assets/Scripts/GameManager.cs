7ad9cb376c800c969abad42adada0381bd52adba
Adam Szi <szi.adam@simonyi.bme.hu>
2019-10-23 17:39:52 +0200

    added menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,23 +3,31 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class GameOverManager : MonoBehaviour {
+public class GameManager : MonoBehaviour {
     public PlayerHealth player_one;
     public PlayerHealth player_two;
 
     public GameObject gameOverPanel;
     public TextMeshProUGUI winText;
 
+    public GameObject continueButton;
+    private Navigation none;
+
     [SerializeField]
     public static bool gameOver;
+    [SerializeField]
+    public static bool paused;
 
     private void Awake() {
-        GameOverManager.gameOver = false;
+        GameManager.gameOver = false;
         gameOverPanel.SetActive(false);
+
+        none = new Navigation();
+        none.mode = Navigation.Mode.None;
     }
 
     private void FixedUpdate() {
-        if (!GameOverManager.gameOver) {
+        if (!GameManager.gameOver) {
             if (player_one.IsDead) {
                 winText.text = "Red won!";
             }
@@ -46,14 +54,40 @@ public class GameOverManager : MonoBehaviour {
         }
     }
 
-    private void GameOver() {
-        GameOverManager.gameOver = true;
-        gameOverPanel.SetActive(true);
+    private void Update() {
+        if (!gameOver && !paused && Input.GetButton("Pause")) {
+            PauseGame();
+        } else if (!gameOver && paused && Input.GetButton("Pause")) {
+            Continue();
+        }
     }
 
-    private void Update() {
-        if (GameOverManager.gameOver && Input.GetButton("Restart")) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    private void PauseGame() {
+        if (gameOver) {
+            continueButton.SetActive(false);
         }
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Continue() {
+        if (!gameOver) {
+            Time.timeScale = 1f;
+            gameOverPanel.SetActive(false);
+        }
+    }
+
+    private void GameOver() {
+        GameManager.gameOver = true;
+        PauseGame();
+    }
+
+    public void Restart() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit() {
+        SceneManager.LoadScene(0);
     }
 }
